1f13e47c46316574357445e7a6eba5d32a291b9b
robdeer1 <r_deer@fanshaweonline.ca>
2024-02-08 20:45:36 -0500

    added content

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "3",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "6",
+    name: "airpods wireless bluetooth Headphones",
+    image: "images/airpods.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit mauris maecenas eleifend, laoreet sollicitudin turpis nascetur nulla leo augue urna primis eros, nam sagittis commodo conubia montes inceptos elementum ridiculus proin.",
+    brand: "apple",
+    category: "electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+export default products;
